2bf81e77ef1ceb9fba0a59f3b21cbf75f65bc2dd
Daniel Beaton <D.Beaton@hotmail.com>
2020-10-04 18:16:18 -0230

    Changed how image sizes are calculated

--- scripts/script.js
+++ scripts/script.js
@@ -66,12 +66,12 @@ function generatorHTML([h,v]) {
   `;
 }
 
-function randomNumber(limit) {
-  return Math.floor(Math.random() * limit);
+function randomNumber(max, min) {
+  if (min === undefined) {
+    return Math.floor(Math.random() * max);
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-
-
 
 
 
